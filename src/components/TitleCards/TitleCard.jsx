b788a472b8398ff7a23821a23b98bc3f24c1cf10
Your Name <you@example.com>
2025-01-17 22:11:10 +0530

    Adding React Netflix clone project folder structure changes and app mouting

---
+++ src/components/TitleCards/TitleCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCard.css'
+
+const TitleCard = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCard
